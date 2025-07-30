f5c14d8f5468b54948bd4350884b56f3d299c533
meskat03 <mhakash796@gmail.com>
2025-07-30 16:04:48 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,22 +5,17 @@ using namespace std;
 
 int main() {
     string input;
-    cout << "Enter a line: ";
-    getline(cin, input);
+    cout << "Enter an expression: ";
+    cin >> input;
 
-    if (input.starts_with("//")) {
-        cout << "Single line comment\n";
-    }
-    else if (input.starts_with("/*")) {
-        if (input.size() >= 4 && input.ends_with("*/")) {
-            cout << "Multi-line comment\n";
-        } else {
-            cout << "Possibly part of a multi-line comment\n";
+    cout << "Operators found:\n";
+    for (char ch : input) {
+        if (ch == '+' || ch == '-' || ch == '*' ||
+            ch == '/' || ch == '%' || ch == '=') {
+            cout << "Operator: " << ch << endl;
         }
-    }
-    else {
-        cout << "Not a comment\n";
     }
 
     return 0;
 }
+
